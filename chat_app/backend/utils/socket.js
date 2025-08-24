243daf6b3b284ae3aca41fe8f6f39117d82017d4
adityatech084-arch <adityatech084@gmail.com>
2025-08-24 18:22:17 +0530

    Update socket.js

--- chat_app/backend/utils/socket.js
+++ chat_app/backend/utils/socket.js
@@ -10,7 +10,8 @@ let io;
 export const initSocket = (server) =>{
     io = new Server(server,{
         cors:{
-            origin:process.env.FRONTEND_URL
+            origin:process.env.FRONTEND_URL,
+             credentials: true
         }
     });
 
