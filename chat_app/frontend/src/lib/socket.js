77c99fef85bed1060cc6e65385ce708ead9075a8
adityatech084-arch <adityatech084@gmail.com>
2025-08-24 18:20:44 +0530

    Update socket.js

--- chat_app/frontend/src/lib/socket.js
+++ chat_app/frontend/src/lib/socket.js
@@ -1,23 +1,22 @@
-import {io} from 'socket.io-client';
+import { io } from "socket.io-client";
 
-let socket=null;
-
+let socket = null;
 
 export const connectSocket = (userId) => {
-    socket = io(process.env.VITE_API_BASE_URL,{
-        query : {userId}
-    });
+  socket = io(import.meta.env.VITE_API_BASE_URL, {
+    query: { userId },
+    withCredentials: true, // ✅ send cookies
+    transports: ["websocket"], // ✅ force WebSocket
+  });
 
-    return socket;
-}
-
+  return socket;
+};
 
 export const getSocket = () => socket;
 
-
 export const disconnectSocket = () => {
-    if(socket){
-        socket.disconnect();
-        socket =  null;
-    }
-}
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
