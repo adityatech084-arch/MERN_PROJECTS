d96c70f9e3da7a9fa9babc3ea5de739eeb4f49c7
adityatech084-arch <adityatech084@gmail.com>
2025-08-24 17:18:17 +0530

    Update app.js

--- chat_app/backend/app.js
+++ chat_app/backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:process.env.FRONTEND_URL,
     credentials:true,
     methods:["GET","POST","PUT"],
      exposedHeaders: ['Content-Disposition'],
